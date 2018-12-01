ebc84323a6bdcdb44a94661d664e4c5dbb21e5d5
Christopher Tse <chris@chris-tse.com>
2018-12-01 14:59:06 -0600

    Initial commit; finished day 1

---
+++ 2018/day1/main.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+let data = fs.readFileSync('input', 'utf-8').split('\n');
+
+let sum = 0;
+let history = new Set();
+let index = 0;
+
+while(!history.has(sum)) {
+    history.add(sum);
+    sum += parseInt(data[index]);
+    index++;
+    if (index >= data.length) {
+        index = 0;
+    }
+}
+
+console.log(sum);
