89d582c741296d016a6073d68eaae3057bcecce4
Tse, Christopher <ctse@netsuite.com>
2019-12-03 00:56:51 -0600

    Add comments for future reference

--- 2019/day2/day2-rust/src/main.rs
+++ 2019/day2/day2-rust/src/main.rs
@@ -4,27 +4,35 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
+	// Read in input to string
 	let mut f = File::open("input")?;
 	let mut buffer = String::new();
-
 	f.read_to_string(&mut buffer)?;
 
+	// Split string into int vector
 	let original_memory: Vec<u32> = buffer
 		.split(',')
 		.map(|item| item.parse::<u32>().unwrap())
 		.collect();
 
+	// Prepare opcode instruction map
 	let mut instructions: HashMap<u32, Box<dyn Fn(u32, u32, u32, &mut [u32]) -> ()>> =
 		HashMap::new();
 
 	instructions.insert(1, Box::new(&addition));
 	instructions.insert(2, Box::new(&multiplication));
 
+	/**** Part 1 ****/
+
+	// Deep copy memory
 	let mut memory: Vec<u32> = original_memory.clone();
 
+	// Use mutable slice to pass by reference, also instructions by reference (not mutable)
 	exec_program(12, 2, memory.as_mut_slice(), &instructions);
 
 	println!("Part 1: {}", memory[0]);
+
+	/**** Part 2 ****/
 
 	for noun in 0..100 {
 		for verb in 0..100 {
@@ -44,6 +52,15 @@ fn main() -> io::Result<()> {
 	Ok(())
 }
 
+/// Executes opcode program with specified values
+///
+/// # Arguments
+///
+/// * `noun` - Number to replace opcode program position 1
+/// * `verb` - Number to replace opcode program position 2
+/// * `memory` - Current opcode memory buffer to work with
+/// * `instructions` - Reference to hashmap with opcode instructions
+///
 pub fn exec_program(
 	noun: u32,
 	verb: u32,
@@ -70,6 +87,14 @@ pub fn exec_program(
 	}
 }
 
+/// Performs opcode addition and saves the result
+///
+/// # Arguments
+///
+/// * `arg1_pos` - Position of first argument
+/// * `arg2_pos` - Position of second argument
+/// * `dest_pos` - Position to store result in
+/// * `memory` - Current opcode memory buffer to work with
 pub fn addition(arg1_pos: u32, arg2_pos: u32, dest_pos: u32, memory: &mut [u32]) {
 	let arg1_pos = arg1_pos as usize;
 	let arg2_pos = arg2_pos as usize;
@@ -77,6 +102,14 @@ pub fn addition(arg1_pos: u32, arg2_pos: u32, dest_pos: u32, memory: &mut [u32])
 	memory[dest_pos] = memory[arg1_pos] + memory[arg2_pos];
 }
 
+/// Performs opcode multiplication and saves the result
+///
+/// # Arguments
+///
+/// * `arg1_pos` - Position of first argument
+/// * `arg2_pos` - Position of second argument
+/// * `dest_pos` - Position to store result in
+/// * `memory` - Current opcode memory buffer to work with
 pub fn multiplication(arg1_pos: u32, arg2_pos: u32, dest_pos: u32, memory: &mut [u32]) {
 	let arg1_pos = arg1_pos as usize;
 	let arg2_pos = arg2_pos as usize;
