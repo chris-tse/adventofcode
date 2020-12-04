62946416e857940728d166163b260b6c4b6713fa
Chris Tse <chris@chris-tse.com>
2020-12-04 15:59:03 -0600

    Remove lazy_static and use plain references

--- 2020/day2/src/main.rs
+++ 2020/day2/src/main.rs
@@ -1,33 +1,26 @@
-#[macro_use]
-extern crate lazy_static;
-
 extern crate regex;
 use entry::Entry;
 use regex::Regex;
 use std::fs;
 mod entry;
 
-lazy_static! {
-	static ref rg: Regex = {
-		return Regex::new(r"(?P<min>\d+)-(?P<max>\d+)\s(?P<target_letter>\w):\s(?P<password>\w+)")
-			.unwrap();
-	};
-}
-
 fn main() {
 	let file_contents = fs::read_to_string("input").expect("Something went wrong");
 
 	let entries: Vec<&str> = file_contents.split("\n").collect();
 
-	part1(entries.clone());
-	part2(entries.clone());
+	let rg = Regex::new(r"(?P<min>\d+)-(?P<max>\d+)\s(?P<target_letter>\w):\s(?P<password>\w+)")
+		.unwrap();
+
+	part1(entries.clone(), &rg);
+	part2(entries.clone(), &rg);
 }
 
-fn part1(entries: Vec<&str>) {
+fn part1(entries: Vec<&str>, rg: &Regex) {
 	let mut num_valid_passwords = 0;
 
 	for e in entries {
-		let entry = Entry::from(e, &*rg);
+		let entry = Entry::from(e, rg);
 		if entry.is_valid() {
 			num_valid_passwords += 1;
 		}
@@ -36,11 +29,11 @@ fn part1(entries: Vec<&str>) {
 	println!("There are {} valid passwords", num_valid_passwords);
 }
 
-fn part2(entries: Vec<&str>) {
+fn part2(entries: Vec<&str>, rg: &Regex) {
 	let mut num_valid_passwords = 0;
 
 	for e in entries {
-		let entry = Entry::from(e, &*rg);
+		let entry = Entry::from(e, rg);
 		if entry.is_valid_part_2() {
 			num_valid_passwords += 1;
 		}
